66ea3b78da64350547ffaec99734a20428b60995
Cleopatra <cleopatra_personal3000@hotmail.com>
2026-03-06 21:55:35 +0100

    w

--- sections/Gallery.jsx
+++ sections/Gallery.jsx
@@ -77,7 +77,6 @@ const Gallery = () => {
           >
             <img className={styles.image} src={image.src} alt={image.alt} loading="lazy" />
           </button>
-          
         ))}
       </div>
 
